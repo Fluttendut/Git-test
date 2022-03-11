e3c57d9673d1d5db4a06d05cfcba5d91e592b254
Ella Frost <ded_19@hotmail.com>
2022-03-11 10:07:35 +0100

    Got coffee??

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class GitTest
 {
     public static void main(String[] args)
     {
+        GitTest prg = new GitTest();
         System.out.println("Hello world it's a me! MAAAARIO");
-        System.out.println("No its me WARIO!")
+        System.out.println("No its me WARIO!");
+        prg.tired();
+    }
 
+    public void tired()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Do you have any coffee? yes or no?");
+        String coffee = sc.nextLine();
+        if(coffee.equals("no"))
+        {
+            System.out.println("Go make some coffee!");
+        }
+        else
+        {
+            System.out.println("Good for you!");
+        }
     }
 
 }
