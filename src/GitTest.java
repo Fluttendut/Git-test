7a2a0e40f1e7aa99852380a6ba48edbae98fe9ae
HkHino <mpfugl@hotmail.com>
2022-03-11 10:04:46 +0100

    greeting improvement

--- src/GitTest.java
+++ src/GitTest.java
@@ -3,6 +3,7 @@ public class GitTest
     public static void main(String[] args)
     {
         System.out.println("Hello world it's a me! MAAAARIO");
+        System.out.println("No its me WARIO!")
 
     }
 
